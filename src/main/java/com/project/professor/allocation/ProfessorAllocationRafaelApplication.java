34be2c623a73a8d08c85bcf2846db0bc52b5c270
RafaelGeorge <rafaelgeorgedemoraescruz@gmail.com>
2022-08-17 20:56:37 -0300

    add project

---
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationRafaelApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationRafaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationRafaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professor/allocation/ProfessorAllocationRafaelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professor.allocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationRafaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
