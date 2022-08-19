56d641d09bf7c9f67f8468d811ac64c211692a88
RafaelGeorge <rafaelgeorgedemoraescruz@gmail.com>
2022-08-19 14:39:18 -0300

    mudança de nome main class

--- src/main/java/com/project/professor/allocation/ProfessorAllocationBackEnd.java
+++ src/main/java/com/project/professor/allocation/ProfessorAllocationBackEnd.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProfessorAllocationRafaelApplication {
+public class ProfessorAllocationBackEnd {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProfessorAllocationRafaelApplication.class, args);
+		SpringApplication.run(ProfessorAllocationBackEnd.class, args);
 	}
 
 }
