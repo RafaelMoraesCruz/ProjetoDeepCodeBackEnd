c634d952dc0ec4b48f5ed588f0d0fa82e5ebecef
evs <elvysnascimentoevs@gmail.com>
2022-08-21 19:02:32 -0300

    Mudança nas anotações classe DPT

--- src/main/java/com/project/professor/allocation/entity/Department.java
+++ src/main/java/com/project/professor/allocation/entity/Department.java
@@ -1,13 +1,19 @@
 package com.project.professor.allocation.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Department {
 	
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "name", nullable = false)
 	private String name;
 
 	public Department() {
