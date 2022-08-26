8c8691be796843f29dd65161e4a3ee8e7a705951
josé marcos da silva filho <marcosfilho1095@gmail.com>
2022-08-26 17:29:07 -0300

    criação allocationService!

--- src/main/java/com/project/professor/allocation/service/AllocationService.java
+++ src/main/java/com/project/professor/allocation/service/AllocationService.java
@@ -1,7 +1,11 @@
 package com.project.professor.allocation.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
+import com.project.professor.allocation.entity.Allocation;
 import com.project.professor.allocation.repository.AllocationRepository;
 
 @Service
@@ -14,4 +18,63 @@ public class AllocationService {
 		this.allocationRepository = allocationRepository;
 	}
 
+	public List<Allocation> findByProfessorId(Long professorId){
+		return allocationRepository.findByProfessorId(professorId);
+	}
+	
+	public List<Allocation> findByCourseId(Long courseId){
+		return allocationRepository.findByCourseId(courseId);
+	}
+	
+	public Optional<Allocation> findById(Long Id) {
+		return allocationRepository.findById(Id);
+	}
+	
+	public List<Allocation> findAll(){
+		return allocationRepository.findAll();
+	}
+
+    public Allocation create (Allocation allocation) {
+    	allocation.setId(null);
+    	return allocationRepository.save(allocation);
+    	
+    }
+    
+    public Allocation update (Allocation allocation) {
+    	if (allocation.getId() != null && allocationRepository.existsById(allocation.getId())){
+    	return allocationRepository.save(allocation);
+    	}else { 
+    		return null;//exception aqui
+    	}}
+    public void deleteAll () {
+     allocationRepository.deleteAll();
+    }
+    
+    public void deleteById(Long id) {
+    	if (id != null && allocationRepository.existsById(id)) {
+    		allocationRepository.deleteById(id);
+		} else {
+			// exception aqui
+		}
+    }
+    	
+
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
