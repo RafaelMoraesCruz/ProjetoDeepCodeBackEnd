f5556f46f70117ce1fbb79206ffde57ac8815c2b
RafaelMoraesCruz <rafaelgeorge344@gmail.com>
2022-09-04 15:21:53 -0300

    ProfessorController finalizado

--- src/main/java/com/project/professor/allocation/controller/ProfessorController.java
+++ src/main/java/com/project/professor/allocation/controller/ProfessorController.java
@@ -9,14 +9,14 @@ import com.project.professor.allocation.service.exception.ServiceNotFindExceptio
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,8 +33,14 @@ public class ProfessorController {
 
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<List<Professor>> findAll() {
-		List<Professor> professors = professorService.findAll();
+	public ResponseEntity<List<Professor>> findAll(@RequestParam(name = "name", required = false) String name)
+			throws ServiceNotFindException {
+		List<Professor> professors;
+		if (name == null) {
+			professors = professorService.findAll();
+		} else {
+			professors = professorService.findByNameContaining(name);
+		}
 		return new ResponseEntity<>(professors, HttpStatus.OK);
 
 	}
@@ -51,16 +57,73 @@ public class ProfessorController {
 		}
 	}
 
+	@GetMapping("deptid/{department_id}")
+	public ResponseEntity<List<Professor>> findByDepartmentId(@PathVariable(name = "department_id") Long departmentId)
+			throws ServiceNotFindException {
+		List<Professor> professor = professorService.findByDepartmentId(departmentId);
+		if (professor.size() == 0) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<List<Professor>>(professor, HttpStatus.OK);
+		}
+	}
+
 	@GetMapping(value = "/cpf")
 	@ResponseStatus(HttpStatus.OK)
-	public ResponseEntity<Professor> findByCpf(@RequestParam String cpf)
-			throws ServiceNotFindException {
+	public ResponseEntity<Professor> findByCpf(@RequestParam String cpf) throws ServiceNotFindException {
 		Professor professor = professorService.findByCpf(cpf);
 		if (professor != null) {
 			return new ResponseEntity<>(professor, HttpStatus.OK);
 		} else {
-		return new ResponseEntity<>(professor, HttpStatus.NOT_FOUND);
+			return new ResponseEntity<>(professor, HttpStatus.NOT_FOUND);
+		}
 	}
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Professor> save(@RequestBody Professor professor) {
+		try {
+			professor = professorService.save(professor);
+			return new ResponseEntity<Professor>(professor, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+		}
+	}
+	
+	@PutMapping(path = "{/professor_id}",consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Professor> update(@PathVariable(name = "professor_id")Long id, @RequestBody Professor professor){
+		professor.setId(id);
+		try {
+			professor = professorService.update(professor);
+			if(professor == null) {
+				return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+			} else {
+				return new ResponseEntity<>(professor, HttpStatus.OK);
+			}
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	
+	@DeleteMapping(path = "/{professor_id}")
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Void> deleteById(@PathVariable(name = "professor_id")Long id) throws ServiceNotFindException{
+		try {
+			professorService.deleteById(id);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Void> deleteAll(){
+		professorService.deleteAll();
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
 
 }
