3670e06a9b33c2813f008c21c8ee67fc6ea732aa
Elvys Marinho <elvysnascimentoevs@gmail.com>
2022-09-04 22:42:44 -0300

    Add create in CourseController

--- src/main/java/com/project/professor/allocation/controller/CourseController.java
+++ src/main/java/com/project/professor/allocation/controller/CourseController.java
@@ -6,6 +6,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -13,7 +16,7 @@ import com.project.professor.allocation.entity.Course;
 import com.project.professor.allocation.service.CourseService;
 
 public class CourseController {
-	
+
 	private final CourseService courseService;
 
 	public CourseController(CourseService courseService) {
@@ -21,6 +24,7 @@ public class CourseController {
 		this.courseService = courseService;
 	}
 
+	// Read
 	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.OK)
 	public ResponseEntity<List<Course>> findAll(@RequestParam(name = "name", required = false) String name) {
@@ -32,6 +36,31 @@ public class CourseController {
 		}
 		return new ResponseEntity<>(courses, HttpStatus.OK);
 
+	}
+
+	// Read
+	@GetMapping(path = "/{course_id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.OK)
+	public ResponseEntity<Course> findById(@PathVariable(name = "course_id") Long id) {
+		Course course = courseService.findById(id);
+		if (course == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(course, HttpStatus.OK);
+		}
+	}
+
+	// Create
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Course> save(@RequestBody Course course) {
+		try {
+			course = courseService.save(course);
+			return new ResponseEntity<Course>(course, HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+
+		}
 	}
 
 }
